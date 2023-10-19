f3e72e5ca6fb985f50bbf05be7e1de0aedf3069d
Chimi-$ <chimithinley705@gmail.com>
2023-10-19 17:07:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screen: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontfamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
+  },
+  plugins: [],
+}
